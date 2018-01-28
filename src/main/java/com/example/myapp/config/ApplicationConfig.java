6d744152e1830626b45e0e8d99a740aed67191d9
whoamix <mfierek@protonmail.ch>
2018-01-28 17:11:11 +0100

    Delete whole project starting from strach

--- src/main/java/com/example/myapp/config/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapp.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class ApplicationConfig extends Application {
-}
